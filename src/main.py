657ef67e1d227e19e7eb99bb75253c4df9530985
Andreu Cotanda Oliver <cotoli23@gmail.com>
2023-07-18 12:18:07 +0200

    Vcs test

--- src/main.py
+++ src/main.py
@@ -12,6 +12,20 @@ import numpy as np
 
 from scripts.vtk2www_inlet_outlets import default_inlet_outlets
 
+
+class Representation:
+    Points = 0
+    Wireframe = 1
+    Surface = 2
+    SurfaceWithEdges = 3
+
+
+class LookupTable:
+    Rainbow = 0
+    Inverted_Rainbow = 1
+    Greyscale = 2
+    Inverted_Greyscale = 3
+
 # -----------------------------------------------------------------------------
 # Trame setup
 # -----------------------------------------------------------------------------
@@ -23,6 +37,8 @@ state, ctrl = server.state, server.controller
 
 state.trame__title = "File Viewer"
 ctrl.on_server_ready.add(ctrl.view_update)
+
+state.setdefault("active_ui", None)
 
 plotter = pv.Plotter()
 
@@ -42,8 +58,18 @@ def file_handle(file_exchange, **kwargs):
                 f.write(bytes)
             ds = pv.read(path.name)
             ds.compute_normals(inplace=True)
-        plotter.add_mesh(ds, name=file.name)
-        plotter.reset_camera()
+
+
+        intlets, outlets, _outlets_wall_dist = default_inlet_outlets(ds)
+        outlets_border = ds.extract_feature_edges(boundary_edges=True, manifold_edges=False, feature_edges=False)
+        connected_cells = outlets_border.connectivity()
+        num_outlets = len(np.unique(connected_cells["RegionId"]))
+
+        if num_outlets == 2:
+            plotter.add_mesh(ds, name=file.name)
+            plotter.reset_camera()
+        else:
+            print("Not a tube")
     else:
         plotter.clear_actors()
         plotter.reset_camera()
@@ -62,13 +88,142 @@ def normal_visibility_change(normal_visibility, **kwargs):
 
 @server.state.change("inlet_visibility")
 def inlet_visibility_change(inlet_visibility, **kwargs):
-    for _, actor in plotter.actors.items():
-        mesh = actor.mapper.dataset
-        inlet, outlets, outlets_wall_distance = default_inlet_outlets(mesh)
+    ctrl.view_update()
+    #for _, actor in plotter.actors.items():
+    #    mesh = actor.mapper.dataset
+    #    inlet, outlets, outlets_wall_distance = default_inlet_outlets(mesh)
+
+
+@state.change("vcs_k_value")
+def update_vcs_k(vcs_k_value, **kwargs):
+    print("update vcs_k:", vcs_k_value)
+
+@state.change("vcs_r_value")
+def update_vcs_r(vcs_r_value, **kwargs):
+    print("update vcs_r:", vcs_r_value)
+
+@state.change("vcs_l_value")
+def update_vcs_l(vcs_l_value, **kwargs):
+    print("update vcs_t:", vcs_l_value)
+
+@state.change("run_vcs")
+def run(run_vcs, **kwargs):
+    print("run vcs:", run_vcs)
+    print("l,r,k:",state.vcs_l_value, state.vcs_r_value,state.vcs_k_value )
 
 # -----------------------------------------------------------------------------
 # Web App setup
 # -----------------------------------------------------------------------------
+
+def actives_change(ids):
+    _id = ids[0]
+    if _id == "1":  # Mesh
+        state.active_ui = "mesh"
+    elif _id == "2":  # VCS
+        state.active_ui = "vcs"
+    else:
+        state.active_ui = "nothing"
+
+def ui_card(title, ui_name):
+    with vuetify.VCard(v_show=f"active_ui == '{ui_name}'"):
+        vuetify.VCardTitle(
+            title,
+            classes="grey lighten-1 py-1 grey--text text--darken-3",
+            style="user-select: none; cursor: pointer",
+            hide_details=True,
+            dense=True,
+        )
+        content = vuetify.VCardText(classes="py-2")
+    return content
+
+def vcs_card():
+    with ui_card(title="VCS", ui_name="vcs"):
+
+        vuetify.VSlider(
+            # Contour Value
+            v_model=("vcs_l_value", 0.5),
+            min=("vcs_l_min", 2),
+            max=("vcs_l_max", 50),
+            step=("vcs_l_step", 1),
+            label="VCS_L",
+            classes="my-1",
+            hide_details=True,
+            dense=True,
+            thumb_label = True,
+        )
+        vuetify.VSlider(
+            # Contour Value
+            v_model=("vcs_r_value", 0.5),
+            min=("vcs_r_min", 2),
+            max=("vcs_r_max", 50),
+            step=("vcs_r_step", 1),
+            label="VCS_R",
+            classes="my-1",
+            hide_details=True,
+            dense=True,
+            thumb_label = True,
+        )
+        vuetify.VSlider(
+            # Contour Value
+            v_model=("vcs_k_value", 0.5),
+            min=("vcs_k_min", 2),
+            max=("vcs_k_max", 50),
+            step=("vcs_k_step", 1),
+            label="VCS_K",
+            classes="my-1",
+            hide_details=True,
+            dense=True,
+            thumb_label = True,
+        )
+
+        vuetify.VTimeline()
+
+        vuetify.VBtn(
+            label = " Compute VCS ",
+            click = lambda: None,
+            block = True,
+            classes="my-1",
+            rounded = True,
+            )
+
+        vuetify.VTimeline()
+        vuetify.VSelect(
+            # Representation
+            v_model=("vcs_representation", Representation.Surface),
+            items=(
+                "vcs_representations",
+                [
+                    {"text": "Ro", "value": 0},
+                    {"text": "Tau", "value": 1},
+                    {"text": "Sigma", "value": 2},
+                    #{"text": "SurfaceWithEdges", "value": 3},
+                ],
+            ),
+            label="VCS_Axis",
+            hide_details=True,
+            dense=True,
+            outlined=True,
+            classes="pt-1",
+        )
+        with vuetify.VCol(cols="6"):
+            vuetify.VSelect(
+                # Color Map
+                label="Colormap",
+                v_model=("contour_color_preset", LookupTable.Rainbow),
+                items=(
+                    "colormaps",
+                    [
+                        {"text": "Rainbow", "value": 0},
+                        {"text": "Inv Rainbow", "value": 1},
+                        {"text": "Greyscale", "value": 2},
+                        {"text": "Inv Greyscale", "value": 3},
+                    ],
+                ),
+                hide_details=True,
+                dense=True,
+                outlined=True,
+                classes="pt-1",
+            )
 
 state.trame__title = "File Viewer"
 
@@ -90,6 +245,7 @@ with SinglePageWithDrawerLayout(server) as layout:
         vuetify.VProgressLinear(
             indeterminate=True, absolute=True, bottom=True, active=("trame__busy",)
         )
+        vcs_card()
 
     with layout.drawer:
         with vuetify.VContainer(fluid=True):
